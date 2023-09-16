76f56f222b9976bc271cf2e323706d0969b0baf0
mcrchavez <mcrchavez713@gmail.com>
2023-09-16 16:09:56 -0400

    Fix

--- commentscraping.py
+++ commentscraping.py
@@ -11,13 +11,21 @@ def get_site(site_url = ""):
     try:
         page = requests.get(url)
     except Exception as e:
-        print(page.status_code)
+        #Requests failed
         print(e)
 
     return page.content
 
-def get_youtube_comments()
-"""
-Use Soup with the requests object to parse through youtube comments section
-Return list of dict of youtube comments: {username: [comments]}
-"""
+
+#def get_youtube_comments()
+    """
+    Use Soup with the requests object to parse through youtube comments section
+    Return list of dict of youtube comments: {username: [comments]}
+    """
+
+def main():
+    get_site("https://discord.com/channels/@me/1152469841937190984/1152680543482286191")
+
+
+if __name___ == "__main__":
+    main()
