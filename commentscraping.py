cf6f9b72bcb0856b6f9a578472580c60a6cfa55d
mkulkar <mkulkar@gmu.edu>
2023-09-16 15:29:52 -0400

    shell code for scraping

---
+++ commentscraping.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+url = "https://www.youtube.com/watch?v=t0X0gLEsAfI&ab_channel=HashtagUnited"
+page = requests.get(url)
+
+soup = bs(page.content, "html.parser")
