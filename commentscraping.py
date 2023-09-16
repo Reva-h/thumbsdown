eb646d5bdba42a9f09fcc89b049be75bb1afd87e
mcrchavez <mcrchavez713@gmail.com>
2023-09-16 16:16:37 -0400

    Test

--- commentscraping.py
+++ commentscraping.py
@@ -7,28 +7,13 @@ def get_site(site_url = ""):
     Returns content object of site to be injested into BS
     """
     try:
-        page = requests.get(site_url)
+        page = requests.get(url)
     except Exception as e:
         #Requests failed
         print(e)
 
     return page.content
 
-<<<<<<< HEAD
-
-#def get_youtube_comments()
-    """
-    Use Soup with the requests object to parse through youtube comments section
-    Return list of dict of youtube comments: {username: [comments]}
-    """
-
-def main():
-    get_site("https://discord.com/channels/@me/1152469841937190984/1152680543482286191")
-
-
-if __name___ == "__main__":
-    main()
-=======
 def get_youtube_comments():
     ''' get input from user or frontend or something idk'''
 
@@ -42,4 +27,3 @@ Return list of dict of youtube comments: {username: [comments]}
 """
 
 get_youtube_comments()
->>>>>>> refs/remotes/origin/scraping
