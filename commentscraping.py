9841b006a9ac3d52b8e6643e158d289baa5a73b1
mcrchavez <mcrchavez713@gmail.com>
2023-09-16 16:18:15 -0400

    Here

--- commentscraping.py
+++ commentscraping.py
@@ -15,15 +15,23 @@ def get_site(site_url = ""):
     return page.content
 
 def get_youtube_comments():
-    ''' get input from user or frontend or something idk'''
+    """
+    Use Soup with the requests object to parse through youtube comments section
+    Return list of dict of youtube comments: {username: [comments]}
+    """
 
     soup = bs(get_site('https://www.youtube.com/watch?v=t0X0gLEsAfI&ab_channel=HashtagUnited'), "html.parser")
     results = soup.find(id = 'content-text')
     print(results)
 
-"""
-Use Soup with the requests object to parse through youtube comments section
-Return list of dict of youtube comments: {username: [comments]}
-"""
+
+def main():
+    get_site('https://www.youtube.com/watch?v=t0X0gLEsAfI&ab_channel=HashtagUnited')
+
+
+
+if __name__ == "__main__":
+    main()
+
 
 get_youtube_comments()
